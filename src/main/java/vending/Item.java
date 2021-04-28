d79145e3b1ad3d58fed37bdd5641a73f3a1ede45
shahdathosain <shahdat85ict@gmail.com>
2021-04-28 17:16:42 -0400

    Initial commit

---
+++ src/main/java/vending/Item.java
@@ -0,0 +1,32 @@
+package vending;
+
+public enum Item {
+
+    COKE("Coke", 25),
+    PEPSI("Pepsi", 35),
+    SODA("Soda", 45);
+
+    private  String name;
+    private int price;
+
+    Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
